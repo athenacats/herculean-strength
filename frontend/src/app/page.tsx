a6d35b674e8b5ebd8f9f603a69024c00c953ceb8
athenacats <124017569+athenacats@users.noreply.github.com>
2023-09-27 16:23:29 +0300

    Set up welcome page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex flex-col h-100">
+      <h1>Welcome to Herculean Strength</h1>
+      <h3>Your strength journey starts here</h3>
+      <div className="button-container flex">
+        <Link href="/signup">
+          <button className="button">New User</button>
+        </Link>
+        <Link href="/signin">
+          <button className="">Returning User</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
