890c2d3c1e198512831c0abc93ad1c3929c2e047
athenacats <124017569+athenacats@users.noreply.github.com>
2023-09-27 17:03:08 +0300

    Add some css styles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,15 +2,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-100">
-      <h1>Welcome to Herculean Strength</h1>
-      <h3>Your strength journey starts here</h3>
-      <div className="button-container flex">
+    <div className="flex flex-col h-screen justify-around items-center">
+      <h1 className="text-4xl text-center">Welcome to Herculean Strength</h1>
+      <h3 className="text-2xl text-center">
+        Your strength journey starts here
+      </h3>
+      <div className="button-container flex gap-10">
         <Link href="/signup">
-          <button className="button">New User</button>
+          <button className="bg-amber-600 p-2 rounded-xl">New User</button>
         </Link>
         <Link href="/signin">
-          <button className="">Returning User</button>
+          <button className="bg-amber-600 p-2 rounded-xl">
+            Returning User
+          </button>
         </Link>
       </div>
     </div>
