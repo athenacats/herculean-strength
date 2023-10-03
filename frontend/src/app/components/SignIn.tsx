7b0c79ad02c07ce8a4a56c267fc2b9ac3a94fd19
athenacats <124017569+athenacats@users.noreply.github.com>
2023-10-03 19:36:36 +0300

    Complete css

--- frontend/src/app/components/SignIn.tsx
+++ frontend/src/app/components/SignIn.tsx
@@ -1,5 +1,6 @@
 "use client"; // required to tell nextjs that its a client component
 
+import Link from "next/link";
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
@@ -19,14 +20,15 @@ export default function SignIn() {
   };
 
   return (
-    <div>
+    <div className="h-600 flex flex-col justify-around items-center mt-20">
+      <h3 className="text-center text-4xl">Log in to your account</h3>
       <Form
         onSubmit={userLogin}
         className="flex flex-col h-48 justify-around items-center m-auto"
       >
         <Form.Group controlId="email">
           <Form.Control
-            className="border-2  focus:border-amber-400 border-amber-600 "
+            className="border-2 w-52 pl-2 rounded-lg focus:border-amber-400 border-amber-600 "
             type="email"
             value={userEmail}
             onChange={(e) => setUserEmail(e.target.value)}
@@ -36,7 +38,7 @@ export default function SignIn() {
         </Form.Group>
         <Form.Group>
           <Form.Control
-            className="border-2  border-amber-600 focus:border-amber-400"
+            className="border-2 w-52 pl-2 rounded-lg border-amber-600 focus:border-amber-400"
             type="password"
             value={userPassword}
             onChange={(e) => setUserPassword(e.target.value)}
@@ -46,13 +48,23 @@ export default function SignIn() {
         </Form.Group>
 
         <Button
-          className="bg-amber-600 p-2 rounded-xl disabled:opacity-75 disabled:cursor-not-allowed"
+          className="bg-amber-600 w-28 p-2 rounded-xl disabled:opacity-75 disabled:cursor-not-allowed"
           type="submit"
           disabled={!isFormValid()}
         >
           Login
         </Button>
       </Form>
+      <div className="m-100">
+        New customer?
+        <Link
+          href="/signup"
+          className="underline decoration-1 hover:cursor-pointer"
+        >
+          {" "}
+          Create your account
+        </Link>
+      </div>
     </div>
   );
 }
