cf7f1c8de3c4d4d47d2dcb755e72578075f67c03
athenacats <124017569+athenacats@users.noreply.github.com>
2023-10-03 19:06:59 +0300

    Change to React bootstrap elements

--- frontend/src/app/components/SignIn.tsx
+++ frontend/src/app/components/SignIn.tsx
@@ -1,7 +1,7 @@
 "use client"; // required to tell nextjs that its a client component
 
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 export default function SignIn() {
   const [userEmail, setUserEmail] = useState("");
@@ -20,31 +20,39 @@ export default function SignIn() {
 
   return (
     <div>
-      <form onSubmit={userLogin}>
-        <input
-          className="border-2  focus:border-amber-400 border-amber-600 "
-          type="email"
-          value={userEmail}
-          onChange={(e) => setUserEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <input
-          className="border-2  border-amber-600 focus:border-amber-400"
-          type="password"
-          value={userPassword}
-          onChange={(e) => setUserPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        <button
+      <Form
+        onSubmit={userLogin}
+        className="flex flex-col h-48 justify-around items-center m-auto"
+      >
+        <Form.Group controlId="email">
+          <Form.Control
+            className="border-2  focus:border-amber-400 border-amber-600 "
+            type="email"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
+            placeholder="Email"
+            required
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            className="border-2  border-amber-600 focus:border-amber-400"
+            type="password"
+            value={userPassword}
+            onChange={(e) => setUserPassword(e.target.value)}
+            placeholder="Password"
+            required
+          ></Form.Control>
+        </Form.Group>
+
+        <Button
           className="bg-amber-600 p-2 rounded-xl disabled:opacity-75 disabled:cursor-not-allowed"
           type="submit"
           disabled={!isFormValid()}
         >
           Login
-        </button>
-      </form>
+        </Button>
+      </Form>
     </div>
   );
 }
