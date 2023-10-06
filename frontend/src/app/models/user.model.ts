4cabc7986c48317ae1bad072128247d1f21abf42
athenacats <124017569+athenacats@users.noreply.github.com>
2023-10-06 12:12:45 +0300

    Create mongoose user model

---
+++ frontend/src/app/models/user.model.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+  password: string;
+  isAdmin: boolean;
+}
+
+export const UserSchema = new Schema<User>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
