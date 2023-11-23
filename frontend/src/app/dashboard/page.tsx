1b3c7db5178b40a89b3c19c02d1bd04a56133df4
athenacats <124017569+athenacats@users.noreply.github.com>
2023-11-23 08:37:08 +0300

    Create dashboard showing username

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import SignIn from "../components/SignIn";
+import Dashboard from "../components/Dashboard";
+
+export default function page() {
+  return (
+    <div>
+      <Dashboard></Dashboard>
+    </div>
+  );
+}
