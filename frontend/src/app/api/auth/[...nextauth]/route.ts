43a26cb20379b2a88f2f7fdec17dd2f42ff70fe3
athenacats <124017569+athenacats@users.noreply.github.com>
2023-11-28 12:01:29 +0300

    Update packages, disable ipv6

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { dbConnect } from "@/app/lib/mongodb";
 import { User, UserModel } from "@/app/models/user.model";
 import NextAuth from "next-auth/next";
