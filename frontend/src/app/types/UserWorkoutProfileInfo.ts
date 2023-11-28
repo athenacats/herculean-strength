5406a6a9f7661623d39c9d93c3b25b788167bd6e
athenacats <124017569+athenacats@users.noreply.github.com>
2023-11-28 18:12:25 +0300

    Create type file for userworkoutprofile

---
+++ frontend/src/app/types/UserWorkoutProfileInfo.ts
@@ -0,0 +1,22 @@
+import { UserInfo } from "./UserInfo";
+
+export type UserWorkoutProfileInfo = {
+  user: UserInfo;
+  sex: string;
+  age: number;
+  heightUnits: string;
+  weightUnits: string;
+  weight: number;
+  height: number;
+  goals: string;
+  workoutFrequency: string;
+  experience: string;
+  currSquatMax: number;
+  currBenchMax: number;
+  currDeadliftMax: number;
+  goalSquatMax: number;
+  goalBenchMax: number;
+  goalDeadliftMax: number;
+  specialization: string;
+  notifications: boolean;
+};
