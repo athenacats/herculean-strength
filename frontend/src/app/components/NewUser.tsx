6dead16c596bf8a99d168bedd3ec678abb1b8649
athenacats <124017569+athenacats@users.noreply.github.com>
2023-11-29 12:46:44 +0300

    Set up preliminary for logic

--- frontend/src/app/components/NewUser.tsx
+++ frontend/src/app/components/NewUser.tsx
@@ -23,11 +23,76 @@ export default function NewUser() {
   });
 
   const [currentStep, setCurrentStep] = useState(1);
+
+  const handleInput = (e: any) => {
+    const { name, value } = e.target.value;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handlePrevious = () => {
+    setCurrentStep((prevStep) => prevStep - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentStep((prevStep) => prevStep + 1);
+  };
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case 1:
+        return (
+          <div className="flex gap-4">
+            <label className="text-xl">Sex:</label>
+            <input
+              className="border-2 w-52 pl-2 rounded-lg focus:border-amber-400 border-amber-600 "
+              type="text"
+              name="sex"
+              value={formData.sex}
+              onChange={handleInput}
+            ></input>
+          </div>
+        );
+      case 2:
+        return (
+          <div className="flex gap-4">
+            <label className="text-xl">Age:</label>
+            <input
+              className="border-2 w-52 pl-2 rounded-lg focus:border-amber-400 border-amber-600 "
+              type="text"
+              name="age"
+              value={formData.age}
+              onChange={handleInput}
+            ></input>
+          </div>
+        );
+    }
+  };
   return (
     <div className="flex flex-col h-96 pt-10 justify-around items-center">
       <h1 className="text-4xl text-center">
         Let&apos;s answer a few questions to set you up!
       </h1>
+      <h2 className="text-2xl text-center">Step {currentStep}</h2>
+      {renderStep()}
+      <div className="flex gap-6">
+        <button
+          className="bg-amber-600 w-28 p-2 rounded-xl disabled:opacity-75 disabled:cursor-not-allowed"
+          onClick={handlePrevious}
+          disabled={currentStep === 1}
+        >
+          Previous
+        </button>
+        <button
+          className="bg-amber-600 w-28 p-2 rounded-xl disabled:opacity-75 disabled:cursor-not-allowed"
+          onClick={handleNext}
+          disabled={currentStep === 11}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
