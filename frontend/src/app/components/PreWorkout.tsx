dc92ae3440bb2abac0bf75545c431c472f993da7
athenacats <chenalonya@gmail.com>
2024-02-22 16:46:14 +0300

    Set slider for energy levels

--- frontend/src/app/components/PreWorkout.tsx
+++ frontend/src/app/components/PreWorkout.tsx
@@ -1,63 +1,104 @@
 "use client";
 import React, { useState } from "react";
-import { moodOptions } from "../types/preWorkoutTypes";
+import { energyOptions, moodOptions } from "../types/preWorkoutTypes";
 
 export default function PreWorkout() {
   const [mood, setMood] = useState(2);
-  const [label, setLabel] = useState("Okay");
+  const [moodLabel, setMoodLabel] = useState("Okay");
+  const [energy, setEnergy] = useState(2);
+  const [energyLabel, setEnergyLabel] = useState("Okay");
 
   const handleMoodChange = (e: any) => {
-    //console.log(e.target.value, e.target.label);
     const selectedValue = parseInt(e.target.value, 10);
     setMood(selectedValue);
     const selectedLabel = moodOptions.find(
       (option) => option.value === selectedValue
     )?.label!;
-    setLabel(selectedLabel);
+    setMoodLabel(selectedLabel);
   };
 
-  /*const selectedMoodLabel = moodOptions.find(
-    (option) => option.value === mood
-  )?.label;*/
+  const handleEnergyChange = (e: any) => {
+    const selectedValue = parseInt(e.target.value, 10);
+    setEnergy(selectedValue);
+    const selectedLabel = energyOptions.find(
+      (option) => option.value === selectedValue
+    )?.label!;
+    setEnergyLabel(selectedLabel);
+  };
 
   return (
-    <div className="py-6 flex flex-col items-center w-full">
-      <h1 className="font-bold text-xl italic">
-        How do you feel today? Be brutally honest
-      </h1>
-      <div className=" flex flex-col items-center">
-        <div className="">
-          <label className="text-center text-lg font-semibold mb-2 block">
-            Mood:
-          </label>
-          <input
-            type="range"
-            min={0}
-            max={5}
-            step={1}
-            value={mood}
-            id="mood"
-            list="markers"
-            name="mood"
-            onChange={handleMoodChange}
-            className=" h-4 w-72 rounded bg-gray-700 outline-none focus:ring-4 focus:ring-
+    <>
+      <div className="py-6 flex flex-col items-center w-full">
+        <h1 className="font-bold text-xl italic">
+          How do you feel today? Be brutally honest
+        </h1>
+        <div className=" flex flex-col items-center">
+          <div className="">
+            <label className="text-center text-lg font-semibold mb-2 block">
+              Mood:
+            </label>
+            <input
+              type="range"
+              min={0}
+              max={5}
+              step={1}
+              value={mood}
+              id="mood"
+              list="markers"
+              name="mood"
+              onChange={handleMoodChange}
+              className=" h-4 w-72 rounded bg-gray-700 outline-none focus:ring-4 focus:ring-
           purple-300 focus:border-purple-300 shadow-md"
-          />
-          <datalist
-            id="markers"
-            className=" flex relative text-sm justify-between"
-          >
-            {moodOptions.map((option) => (
-              <option
-                key={option.value}
-                label={option.label}
-                value={option.value}
-              />
-            ))}
-          </datalist>
-          <p>Value : {label}</p>
+            />
+            <datalist
+              id="markers"
+              className=" flex relative text-sm justify-between"
+            >
+              {moodOptions.map((option) => (
+                <option
+                  key={option.value}
+                  label={option.label}
+                  value={option.value}
+                />
+              ))}
+            </datalist>
+            <p>Value : {moodLabel}</p>
+          </div>
+        </div>
+        <div className=" flex flex-col items-center">
+          <div className="">
+            <label className="text-center text-lg font-semibold mb-2 block">
+              Energy Levels:
+            </label>
+            <input
+              type="range"
+              min={0}
+              max={5}
+              step={1}
+              value={energy}
+              id="energy"
+              list="markers"
+              name="energy"
+              onChange={handleEnergyChange}
+              className=" h-4 w-72 rounded bg-gray-700 outline-none focus:ring-4 focus:ring-
+          purple-300 focus:border-purple-300 shadow-md"
+            />
+            <datalist
+              id="markers"
+              className=" flex relative text-sm justify-between"
+            >
+              {energyOptions.map((option) => (
+                <option
+                  key={option.value}
+                  label={option.label}
+                  value={option.value}
+                />
+              ))}
+            </datalist>
+            <p>Value : {energyLabel}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- frontend/src/app/types/preWorkoutTypes.ts
+++ frontend/src/app/types/preWorkoutTypes.ts
@@ -3,7 +3,21 @@ export type MoodOption = {
   value: number;
 };
 
+export type EnergyOption = {
+  label: string;
+  value: number;
+};
+
 export const moodOptions: MoodOption[] = [
+  { label: "Very bad", value: 0 },
+  { label: "Bad", value: 1 },
+  { label: "Okay", value: 2 },
+  { label: "Good", value: 3 },
+  { label: "Very good", value: 4 },
+  { label: "Excellent", value: 5 },
+];
+
+export const energyOptions: EnergyOption[] = [
   { label: "Very bad", value: 0 },
   { label: "Bad", value: 1 },
   { label: "Okay", value: 2 },
