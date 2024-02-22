e67a88a70e9af65c9d67bb1600ce8540b83db505
athenacats <chenalonya@gmail.com>
2024-02-22 17:26:09 +0300

    Create route for actual workout

---
+++ frontend/src/app/components/StartWorkout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function StartWorkout() {
+  return <div>Start Workout</div>;
+}
+
+export default StartWorkout;
---
+++ frontend/src/app/start-workout/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import StartWorkout from "../components/StartWorkout";
+
+function page() {
+  return <StartWorkout />;
+}
+
+export default page;
