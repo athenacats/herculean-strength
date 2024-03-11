bb1f44f4682f515064e917daa718d4fba4ada59b
athenacats <chenalonya@gmail.com>
2024-03-11 19:55:09 +0300

    Add variations for accessories

--- frontend/src/app/types/Variations.ts
+++ frontend/src/app/types/Variations.ts
@@ -5,6 +5,16 @@ export interface ExcerciseVariation {
   hard: number;
 }
 
+export interface AccessoriesVariation {
+  name: string;
+  weight: number;
+}
+
+export interface Abdominals {
+  name: string;
+  weight: number;
+}
+
 export const squatVariation: ExcerciseVariation[] = [
   { name: "Competition Squat", easy: 50, medium: 70, hard: 90 },
   { name: "Front Squat", easy: 30, medium: 50, hard: 70 },
@@ -27,4 +37,38 @@ export const deadliftVariation: ExcerciseVariation[] = [
   { name: "Deadlifts with Chains", easy: 60, medium: 80, hard: 80 },
   { name: "Defecit Deadlifts", easy: 60, medium: 70, hard: 80 },
   { name: "Rack Pull Deadlifts", easy: 70, medium: 85, hard: 110 },
+];
+
+export const squatAccessories: AccessoriesVariation[] = [
+  { name: "Goodmorning", weight: 30 },
+  { name: "Leg Press", weight: 60 },
+  { name: "Hack Squat", weight: 60 },
+  { name: "Leg Extension", weight: 30 },
+  { name: "Bulgarian Split Squats", weight: 20 },
+  { name: "Calf Raises", weight: 20 },
+];
+
+export const deadliftAccessories: AccessoriesVariation[] = [
+  { name: "Hyperextensions", weight: 20 },
+  { name: "Leg Curls", weight: 30 },
+  { name: "Romanian Deadlift", weight: 40 },
+  { name: "Hip Thrust", weight: 60 },
+  { name: "Seated Cable Row", weight: 40 },
+  { name: "Lat Pulldown", weight: 40 },
+];
+
+export const benchAccessories: AccessoriesVariation[] = [
+  { name: "Incline Press", weight: 50 },
+  { name: "Dumbell Press", weight: 20 },
+  { name: "Overhead Press", weight: 50 },
+  { name: "Dumbell Curl", weight: 20 },
+  { name: "Tricep Pushdown", weight: 20 },
+  { name: "Overhead Tricep Extension", weight: 20 },
+];
+
+export const abdominals: Abdominals[] = [
+  { name: "Hanging Pike", weight: 0 },
+  { name: "Paloff Press", weight: 10 },
+  { name: "Sit up", weight: 0 },
+  { name: "Landmine Twist", weight: 10 },
 ];
