61f13d751561f04395052eb18759d65efb29d206
athenacats <chenalonya@gmail.com>
2024-03-11 19:26:51 +0300

    Update the type file to be percentage of easy, medium and hard

--- frontend/src/app/types/SquatVariations.ts
+++ frontend/src/app/types/SquatVariations.ts
@@ -6,25 +6,25 @@ export interface ExcerciseVariation {
 }
 
 export const squatVariation: ExcerciseVariation[] = [
-  { name: "Competition Squat", easy: 60, medium: 80, hard: 100 },
-  { name: "Front Squat", easy: 40, medium: 60, hard: 80 },
-  { name: "Squat with Chains", easy: 60, medium: 80, hard: 90 },
-  { name: "Pin Squat", easy: 60, medium: 80, hard: 100 },
-  { name: "Pause Squat", easy: 60, medium: 80, hard: 100 },
+  { name: "Competition Squat", easy: 50, medium: 70, hard: 90 },
+  { name: "Front Squat", easy: 30, medium: 50, hard: 70 },
+  { name: "Squat with Chains", easy: 40, medium: 55, hard: 70 },
+  { name: "Pin Squat", easy: 50, medium: 70, hard: 80 },
+  { name: "Pause Squat", easy: 50, medium: 70, hard: 80 },
 ];
 
 export const benchVariation: ExcerciseVariation[] = [
-  { name: "Competition Bench Press", easy: 60, medium: 80, hard: 100 },
-  { name: "Elevated Legs Bench Press", easy: 40, medium: 60, hard: 80 },
-  { name: "Bench Press with Chains", easy: 60, medium: 80, hard: 90 },
-  { name: "Pin Bench Press", easy: 60, medium: 80, hard: 100 },
-  { name: "Narrow Grip Bench Press", easy: 60, medium: 80, hard: 100 },
+  { name: "Competition Bench Press", easy: 50, medium: 70, hard: 90 },
+  { name: "Elevated Legs Bench Press", easy: 30, medium: 50, hard: 70 },
+  { name: "Bench Press with Chains", easy: 30, medium: 50, hard: 70 },
+  { name: "Pin Bench Press", easy: 50, medium: 65, hard: 80 },
+  { name: "Narrow Grip Bench Press", easy: 50, medium: 50, hard: 70 },
 ];
 
 export const deadliftVariation: ExcerciseVariation[] = [
-  { name: "Competition Deadlifts", easy: 60, medium: 80, hard: 100 },
-  { name: "Deadlifts with Pause at Knees", easy: 40, medium: 60, hard: 80 },
-  { name: "Deadlifts with Chains", easy: 60, medium: 80, hard: 90 },
-  { name: "Defecit Deadlifts", easy: 60, medium: 80, hard: 100 },
-  { name: "Rack Pull Deadlifts", easy: 60, medium: 80, hard: 100 },
+  { name: "Competition Deadlifts", easy: 60, medium: 70, hard: 90 },
+  { name: "Deadlifts with Pause at Knees", easy: 60, medium: 70, hard: 80 },
+  { name: "Deadlifts with Chains", easy: 60, medium: 80, hard: 80 },
+  { name: "Defecit Deadlifts", easy: 60, medium: 70, hard: 80 },
+  { name: "Rack Pull Deadlifts", easy: 70, medium: 85, hard: 110 },
 ];
