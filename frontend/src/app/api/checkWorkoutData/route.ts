2db6b9aa85690f370e503753391d2fe14d72c816
athenacats <chenalonya@gmail.com>
2024-04-16 13:55:40 +0300

    Route to check for previous workouts

---
+++ frontend/src/app/api/checkWorkoutData/route.ts
@@ -0,0 +1,43 @@
+import { dbConnect } from "@/app/lib/mongodb";
+import { UserWorkoutProfileModel } from "@/app/models/userWorkoutProfile.model";
+import dotenv from "dotenv";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { NextRequest, NextResponse } from "next/server";
+import { WorkoutDetailsModel } from "@/app/models/workoutDetails";
+dotenv.config();
+
+export async function GET(
+  error: any,
+  req: NextRequest,
+  res: NextResponse,
+  _next: any
+) {
+  console.log("here workout profile");
+  try {
+    await dbConnect();
+
+    const session = await getServerSession(authOptions);
+
+    console.log("session: " + session);
+
+    if (!session || !session.user) {
+      return NextResponse.json({ error: "Not registered" });
+    }
+
+    const user = session?.user?.email;
+    console.log(user);
+
+    const userFound = await WorkoutDetailsModel.findOne({ user: user });
+    console.log(userFound);
+
+    if (userFound) {
+      return NextResponse.json(userFound);
+    } else {
+      return NextResponse.json({ error: "No previous workouts" });
+    }
+  } catch (error) {
+    console.error("Error during save operation:", error);
+    return NextResponse.json({ message: "An error occurred" });
+  }
+}
